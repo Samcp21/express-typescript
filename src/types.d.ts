a9e394d829570dd8ea33522aa8e1dd09e6231d35
samcp210@gmail.com <samcp210@gmail.com>
2023-12-03 20:15:44 -0500

    init

---
+++ src/types.d.ts
@@ -0,0 +1,19 @@
+export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "stormy";
+export type Visibility = "great" | "good" | "ok" | "poor";
+
+export interface DiaryEntry {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment: string;
+}
+
+// export type NonSensitiveInfoDiaryEntry = Pick<
+//   DiaryEntry,
+//   "id" | "data" | "weather" | "visibility"
+// >;
+
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, "comment">;
+
+export type NewDiaryEntry = Omit<DiaryEntry, "id">;
